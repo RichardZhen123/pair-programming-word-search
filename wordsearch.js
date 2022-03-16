05fa9abadaba5d1a1cc231890767f28358ba1b2c
RichardZhen123 <richardzhen5555@gmail.com>
2022-03-16 21:23:51 +0000

    update

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    let letters2 = [];
+    for (const iterator of letters[0]) {
+      letters2.push([]);
     }
-}
-
-module.exports = wordSearch
+    for (let rows = 0; rows < letters.length; rows++) {
+      for (let columns = 0; columns < letters[rows].length; columns++) {
+        letters2[columns].push(letters[rows][columns]);
+      }
+    }
+    
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = letters2.map(ls => ls.join(''));
+    let axis = [horizontalJoin, verticalJoin];
+    for (let i = 0; i < axis.length; i++) {
+      for (const l of axis[i]) {
+        if (l.includes(word)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
